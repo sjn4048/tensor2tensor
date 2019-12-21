efe09c196d07c54a22d466cbc19edaf69e57c310
Jianing Shi <3160105216@zju.edu.cn>
2019-12-22 01:05:03 +0800

    Updated SUBWORD ver for iwslt14

--- usr/iwslt14_problem.py
+++ usr/iwslt14_problem.py
@@ -65,7 +65,7 @@ class TranslateDeEnIwslt(translate.TranslateProblem):
 
   def vocab_data_files(self):
     """Files to be passed to get_or_generate_vocab."""
-    return [[None, ('train.en', 'train.de')]]
+    return [['None', ('train.en', 'train.de')]]
 
   def generate_samples(self, data_dir, tmp_dir, dataset_split):
     """Instance of token generator for the IWSLT de->en task, training set."""
