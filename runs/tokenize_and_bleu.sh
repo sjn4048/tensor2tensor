35760e5474501fe0aec3c71f1b54bbfeee27d8bc
Jianing Shi <3160105216@zju.edu.cn>
2019-12-22 21:30:24 +0800

    Updated tokenize&bleu script for subwords

--- runs/tokenize_and_bleu.sh
+++ runs/tokenize_and_bleu.sh
@@ -5,10 +5,9 @@ exp_name=${exp_name}
 
 for ((steps = eval_every; steps <= max_steps; steps += eval_every))
 do
-  target=./checkpoints/${exp_name}/evals/${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.targets
-  output=./checkpoints/${exp_name}/evals/${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.decodes
-  perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $output >  "${output}.tok"
-  perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $target >  "${target}.tok"
-
-  perl mosesdecoder/scripts/generic/multi-bleu.perl ${target}.tok < ${output}.tok
+  target=${ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.targets"}
+  target=${ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.decodes"}
+  perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $output >  "${output}.tok" 2>&1 > /dev/null
+  perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $target >  "${target}.tok" 2>&1 > /dev/null
+  echo "${step}: ${perl mosesdecoder/scripts/generic/multi-bleu.perl ${target}.tok < ${output}.tok | tail -n 1}"
 done
