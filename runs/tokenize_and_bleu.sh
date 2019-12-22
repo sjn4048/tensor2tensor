3cf5c017bdb5e61731cb9c4362e06403deb91334
Jianing Shi <3160105216@zju.edu.cn>
2019-12-22 19:49:17 +0800

    Updated tokenize&bleu script for subwords

--- runs/tokenize_and_bleu.sh
+++ runs/tokenize_and_bleu.sh
@@ -1,10 +1,12 @@
 max_steps=${max_steps:-100000}
 eval_every=${eval_every:-2000}
 
+exp_name=${exp_name}
+
 for ((steps = eval_every; steps <= max_steps; steps += eval_every))
 do
-  target=${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.targets
-  output=${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.decodes
+  target=./checkpoints/${exp_name}/evals/${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.targets
+  output=./checkpoints/${exp_name}/evals/${steps}.transformer.transformer_small.translate_de_en_iwslt.beam4.alpha1.0.decodes
   perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $output >  "${output}.tok"
   perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $target >  "${target}.tok"
 
