d7be955006183a6ba19c0f05d1d5414bc109571e
Jianing Shi <3160105216@zju.edu.cn>
2019-12-22 21:44:45 +0800

    Updated tokenize&bleu script for subwords

--- runs/tokenize_and_bleu.sh
+++ runs/tokenize_and_bleu.sh
@@ -5,8 +5,12 @@ exp_name=${exp_name}
 
 for ((steps = eval_every; steps <= max_steps; steps += eval_every))
 do
-  target=${ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.targets"}
-  target=${ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.decodes"}
+  target=$(ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.targets")
+  output=$(ls -1L "./checkpoints/${exp_name}/evals/${steps}.*.decodes")
+  if [ -z "$target" ]; then
+    echo "${step}: No output"
+    continue
+  fi
   perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $output >  "${output}.tok" 2>&1 > /dev/null
   perl mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < $target >  "${target}.tok" 2>&1 > /dev/null
   echo "${step}: ${perl mosesdecoder/scripts/generic/multi-bleu.perl ${target}.tok < ${output}.tok | tail -n 1}"
